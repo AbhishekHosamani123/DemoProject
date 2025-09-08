02b64c618a68cbeff9618877e31ba9484e7587b3
Kumar Abhinav <abhinavasylum@gmail.com>
2025-09-08 15:33:32 +0000

    error reaching that server

--- next.config.ts
+++ next.config.ts
@@ -24,16 +24,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '**/*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
 };
 
 export default nextConfig;
