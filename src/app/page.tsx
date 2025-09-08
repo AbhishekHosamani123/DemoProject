cd6b799ad839d466c6739492896250220cebb099
Kumar Abhinav <abhinavasylum@gmail.com>
2025-09-08 20:47:04 +0000

    same horizontal line just go 2 steps left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,8 +55,8 @@ export default function Home() {
   };
 
   return (
-    <main className="flex-1 flex flex-col items-center justify-center p-4 pt-48">
-      <div className="w-full max-w-7xl mx-auto">
+    <main className="flex-1 flex flex-col items-center justify-start p-4 pt-48">
+       <div className="relative w-full max-w-7xl" style={{ left: '-2rem' }}>
         {data.length === 0 ? (
            <div className="relative max-w-xl mx-auto flex flex-col items-center gap-8">
             <FileUploader onFileUpload={handleFileUpload} />
