216b04ca44e52afd9226e0d7cc48d68b385cc55d
Kumar Abhinav <abhinavasylum@gmail.com>
2025-09-08 16:19:59 +0000

    very good but again chatbot is been moved from where it was ;(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,16 +31,14 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased bg-background text-foreground" suppressHydrationWarning>
-        <div className="relative flex min-h-screen flex-col">
-            <AppShell>
-                <div className="flex-1">
-                    {children}
-                    <div className="absolute inset-0 -z-50 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-                        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary/10 blur-[100px]"></div>
-                    </div>
-                </div>
-            </AppShell>
-        </div>
+        <AppShell>
+          <div className="relative flex-1">
+            {children}
+            <div className="absolute inset-0 -z-50 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+              <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary/10 blur-[100px]"></div>
+            </div>
+          </div>
+        </AppShell>
         <Toaster />
         <Chatbot />
       </body>
